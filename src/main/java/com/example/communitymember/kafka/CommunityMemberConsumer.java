5aa25088e7037c873737c1ae7206a40df6270114
donymyounglee <pajang1515@daum.net>
2023-09-25 14:33:42 +0900

    dlt 설정 완료

--- src/main/java/com/example/communitymember/kafka/CommunityMemberConsumer.java
+++ src/main/java/com/example/communitymember/kafka/CommunityMemberConsumer.java
@@ -1,24 +1,40 @@
 package com.example.communitymember.kafka;
 
+import com.example.communitymember.common.RestError;
+import com.example.communitymember.common.RestResult;
 import com.example.communitymember.domain.request.CommunityMemberReqeust;
 import com.example.communitymember.service.CommunityMemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CommunityMemberConsumer {
     private final CommunityMemberService communityMemberService;
     @KafkaListener(topics = TopicConfig.communityMember)
     public void listen(CommunityMemberReqeust communityMemberReqeust) {
+        System.out.println("consumer : " + communityMemberReqeust);
+//        throw new IllegalArgumentException("test");
+
         communityMemberService.saveCommunityMember(communityMemberReqeust);
+    }
+
+    @KafkaListener(topics = TopicConfig.communityMemberDLT)
+    public void dltListen(byte[] in) {
+        log.info("dlt : " + new String(in));
     }
 
     @KafkaListener(topics = TopicConfig.communityMemberUpdate)
     public void updateCommunityMember(CommunityMemberReqeust communityMemberReqeust) {
-        System.out.println("멤버 수정 들오긴함");
+
         communityMemberService.updateMemberInCommunityMember(communityMemberReqeust);
     }
+
+
 
 }
--- src/main/java/com/example/communitymember/kafka/TopicConfig.java
+++ src/main/java/com/example/communitymember/kafka/TopicConfig.java
@@ -1,10 +1,19 @@
 package com.example.communitymember.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TopicConfig {
     public final static String communityMember = "communityMember";
     public final static String communityMemberUpdate = "communityMemberUpdate";
+
+    public final static String communityMemberDLT = "communityMember.DLT";
+
+    @Bean
+    public NewTopic topicDLT() {
+        return new NewTopic(communityMemberDLT, 1, (short) 1);
+    }
 
 }
