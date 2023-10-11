55ca76e3c8352dcc0b03802b7381d61414f61209
dokyeong99 <dlehrud451@gmail.com>
2023-10-11 20:56:06 +0900

    dk_refactoring

--- src/main/java/com/example/communitymember/kafka/CommunityMemberConsumer.java
+++ src/main/java/com/example/communitymember/kafka/CommunityMemberConsumer.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CommunityMemberConsumer {
     private final CommunityMemberService communityMemberService;
+    @RetryableTopic
     @KafkaListener(topics = TopicConfig.communityMember)
     public void listen(CommunityMemberReqeust communityMemberReqeust) {
         System.out.println("consumer : " + communityMemberReqeust);
@@ -24,15 +27,17 @@ public class CommunityMemberConsumer {
         communityMemberService.saveCommunityMember(communityMemberReqeust);
     }
 
-    @KafkaListener(topics = TopicConfig.communityMemberDLT)
-    public void dltListen(byte[] in) {
-        log.info("dlt : " + new String(in));
-    }
-
-    @KafkaListener(topics = TopicConfig.communityMemberUpdate)
+    @RetryableTopic
+    @KafkaListener(topics = TopicConfig.memberUpdate)
     public void updateCommunityMember(CommunityMemberReqeust communityMemberReqeust) {
 
         communityMemberService.updateMemberInCommunityMember(communityMemberReqeust);
+    }
+
+    @DltHandler
+    public void processDltMessage(String dltMessage) {
+        // DLT 토픽에서 메시지를 처리합니다. (예: 로깅 또는 추가 조사)
+        System.out.println("DLT에서 메시지 수신: " + dltMessage);
     }
 
 
--- src/main/java/com/example/communitymember/kafka/TopicConfig.java
+++ src/main/java/com/example/communitymember/kafka/TopicConfig.java
@@ -7,13 +7,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class TopicConfig {
     public final static String communityMember = "communityMember";
-    public final static String communityMemberUpdate = "communityMemberUpdate";
 
     public final static String communityMemberDLT = "communityMember.DLT";
+
+    public final static String memberUpdate = "memberUpdate";
+
+    public final static String memberUpdateDLT = "memberUpdate.DLT";
+
 
     @Bean
     public NewTopic topicDLT() {
         return new NewTopic(communityMemberDLT, 1, (short) 1);
     }
+
+    @Bean
+    public NewTopic memberUpdateDLT() {
+        return new NewTopic(memberUpdateDLT, 1, (short) 1);
+    }
+
 
 }
